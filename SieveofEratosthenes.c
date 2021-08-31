d3d988bf3965df0fe1be7d8bd185ef86947120be
Sumit Kumar Thakur <sumitkrthakur18@gmail.com>
2021-08-31 19:18:14 +0530

    SieveofEratosthenes in c added

---
+++ SieveofEratosthenes.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(){
+	
+	int array[100];
+	int i,j;
+	for(i=0;i<100;i++){
+		array[i]=i+1;
+	}
+//	for(i=0;i<100;i++){
+//		printf("%d\n",array[i]);
+//	}
+	//j=3;
+	// sieve of eratosthenes
+	for (i=1;i<=pow(100,0.5);i++){
+		j=i+2;
+		if(array[i]!=0){
+		
+		while(j<100){
+			if(array[j]%array[i]==0){
+				array[j]=0;
+			}
+			j=j+2;
+		}
+	}
+		
+	}
+	
+	
+	for(i=1;i<100;i++){
+		if(array[i]!=0)
+		printf("%d\n",array[i]);
+	}	
+}
